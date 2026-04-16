70b2085c28629c7b8bd0c4ea41f0b355fc18fc4d
mperuzzo <mperuzzo@softexpert.com>
2026-04-16 15:25:56 -0300

    feat: feita a componentização da tela de History

--- front/src/components/history/HistoryTable.jsx
+++ front/src/components/history/HistoryTable.jsx
@@ -1,9 +1,35 @@
 import './styles/HistoryTable.css'
-function HistoryTable(){
-    return(
-        <>
-        <h1>History</h1>
-        </>
-    )
+import '../basics/styles/ProductTable.css';
+
+function HistoryTable({ details, onDetail }) {
+  return (
+    <div className="tableHistory">
+      <table>
+        <thead>
+          <tr>
+            <th className="thCode">Code</th>
+            <th className="thTax" id="line1">Tax</th>
+            <th className="thTotal" id="line1">Total</th>
+            <th className="thActions" id="line1">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {details.map((order) => (
+            <tr key={order.code} className="product1">
+              <td>{order.code}</td>
+              <td>R$ {order.tax}</td>
+              <td>R$ {order.total}</td>
+              <td className="tdButton1">
+                <button className="detail1" onClick={() => onDetail(order.code)}>
+                  Detail
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
 }
+
 export default HistoryTable
--- front/src/components/pages/History.jsx
+++ front/src/components/pages/History.jsx
@@ -1,9 +1,20 @@
-import HistoryTable from "../history/HistoryTable"
-function History(){
-    return(
-        <>
-        <HistoryTable/>
-        </>
-    )
+import HistoryTable from "../history/HistoryTable";
+import { useState } from "react";
+function History() {
+  const [details, setDetails] = useState([
+    { code: 1, tax: 10, total: 100 },
+    { code: 2, tax: 5, total: 50 },
+    { code: 3, tax: 20, total: 200 },
+  ]);
+
+  function viewDetail(code) {
+    const filtered = details.filter((d) => d.code !== code);
+    setDetails(filtered);
+  }
+  return (
+    <>
+      <HistoryTable details={details} onDetail={viewDetail} />
+    </>
+  );
 }
-export default History
+export default History;
